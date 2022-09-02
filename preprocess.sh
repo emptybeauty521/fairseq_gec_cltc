3691f6a71aacc41b5432d37bec84c1d421d3b599
刘艳杰 <37999455+emptybeauty521@users.noreply.github.com>
2022-09-02 16:01:43 +0800

    Update

--- preprocess.sh
+++ preprocess.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
 
-set -e
-set -x
+RAW_DATA=./data/cltc/pretrain
+BIN_DATA=./data/cltc/pretrain/bin
 
-RAW_DATA=./data/cltc/finetune
-BIN_DATA=./data/cltc/finetune/bin
-
-# set copy params
 copy_params='--copy-ext-dict'
 
-# set common params between train/valid
-common_params='--source-lang src --target-lang tgt  
+common_params='--source-lang src --target-lang tgt
 --padding-factor 1 
 --srcdict ./data/cltc/pretrain/dict.src.txt
 --joined-dictionary 
 '
 
-splits_num=1
+# 分片数量，并行处理的进程数量
+splits_num=10
 threads_num=12
+
 train_file="train"
 valid_file="valid"
 
-# preprocess train
+# 多进程预处理训练数据
+# 文件前缀为：train{n}_0，其中n为1、2、3...n
 for ((split = 1; split <= $splits_num; split++))
 do
     trainpref=$RAW_DATA/$train_file$split"_0"
@@ -41,13 +39,12 @@ do
     fi
 done
 
-# validpref=$RAW_DATA/$valid_file
-#
-# # preprocess valid, 不需要alignfile
-# nohup python preprocess.py \
-# $common_params \
-# $copy_params \
-# --validpref $validpref \
-# --destdir $BIN_DATA \
-# --output-format binary \
-# > $BIN_DATA/preprocess_$valid_file.log &
+# 预处理验证数据，可不用对齐信息
+validpref=$RAW_DATA/$valid_file
+nohup python preprocess.py \
+$common_params \
+$copy_params \
+--validpref $validpref \
+--destdir $BIN_DATA \
+--output-format binary \
+> $BIN_DATA/preprocess_$valid_file.log &
