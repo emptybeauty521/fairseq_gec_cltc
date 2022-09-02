57a3c6d2beb1bba1731bfdbcda70373df352e4f2
刘艳杰 <37999455+emptybeauty521@users.noreply.github.com>
2022-09-02 16:35:12 +0800

    Update

--- pretrain.sh
+++ pretrain.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 
-
-#--clip-norm 5 \
-#以n个batch分别对深度神经网络做前向传播、计算损失、后向传播而计算网络参数的梯度，累积n个batch的梯度，最后更新参数；
-#每个batch对参数的梯度都是独立计算的
-#--use-sentence-copying \
-#--use-encoder-classification \
-
-
-#DATA_BIN=./data/bin/test
-DATA_BIN=./data/cltc/pretrain/seed2
+# --use-encoder-classification使用Token级标记任务
+# --positive-label-weight错误Token损失的权重
+DATA_BIN=./data/cltc/pretrain/bin
 CUDA_VISIBLE_DEVICES=3 nohup python train.py $DATA_BIN \
---save-dir ./model/pretrain_seed2_cltc \
+--save-dir ./model/pretrain_cltc \
 --max-epoch 10 \
 --batch-size 128 \
 --max-tokens 14464 \
@@ -20,7 +13,7 @@ CUDA_VISIBLE_DEVICES=3 nohup python train.py $DATA_BIN \
 --valid-subset valid --skip-invalid-size-inputs-valid-test \
 --arch transformer \
 --optimizer adam --adam-betas '(0.9, 0.98)' \
---lr 0.0002 --min-lr 1e-09 --lr-scheduler inverse_sqrt \
+--lr 0.0003 --min-lr 1e-09 --lr-scheduler inverse_sqrt \
 --warmup-init-lr 1e-07 --warmup-updates 16000 \
 --save-interval-updates 19527 \
 --dropout 0.2 --relu-dropout 0.2 --attention-dropout 0.2 --copy-attention-dropout 0.2 \
@@ -32,10 +25,9 @@ CUDA_VISIBLE_DEVICES=3 nohup python train.py $DATA_BIN \
 --share-all-embeddings \
 --no-progress-bar \
 --log-interval 20 \
---positive-label-weight 3 \
+--positive-label-weight 2.5 \
 --copy-attention --copy-attention-heads 1 \
 --no-ema \
 --use-encoder-classification \
---label-gen-loss-rate 1 \
---seed 2 \
-> log/pretrain_seed2_cltc.log &
+--seed 1 \
+> log/pretrain_cltc.log &
