cacaa69c3d47a195cd7ac6549d8a0090fe939080
刘艳杰 <37999455+emptybeauty521@users.noreply.github.com>
2022-09-05 09:27:36 +0800

    Update

--- train_sec.sh
+++ train_sec.sh
@@ -1,28 +1,21 @@
 #!/usr/bin/env bash
 
-#--clip-norm 5 \
-#--use-sentence-copying \
-#--use-encoder-classification \
-#--label-gen-loss-rate 5 \
-#--raw-text \
-#--pretrained-model ./model/pre_dae_opt_lb_dp/checkpoint_best.pt \
-#--restore-file ft_dae_opt_lb_bs2_5_norm_err2.pt \
-
-
-DATA_BIN=./data/cltc/finetune/cged_bin
+# --use-encoder-classification使用Token级标记任务
+# --positive-label-weight错误Token损失的权重
+DATA_BIN=./data/cltc/finetune/cged_all_bin
 CUDA_VISIBLE_DEVICES=1 nohup python train.py $DATA_BIN \
---save-dir ./model/ft_lang8_all_cged_cltc3_1 \
+--save-dir ./model/ft_lang8_all_cged_all_cltc7_7 \
 --max-epoch 5 \
 --batch-size 128 \
 --max-tokens 14464 \
 --update-freq 2 \
 --train-subset train1_0 \
 --valid-subset valid --skip-invalid-size-inputs-valid-test \
---pretrained-model ./model/ft_lang8_all_cltc3_1/checkpoint4.pt \
+--pretrained-model ./model/ft_lang8_all_cltc7_7/checkpoint4.pt \
 --arch transformer \
 --optimizer adam --adam-betas '(0.9, 0.98)' \
 --lr 0.00001 --min-lr 1e-09 --lr-scheduler inverse_sqrt \
---warmup-init-lr 1e-06 --warmup-updates 45 \
+--warmup-init-lr 1e-06 --warmup-updates 59 \
 --dropout 0.2 --relu-dropout 0.2 --attention-dropout 0.2 --copy-attention-dropout 0.2 \
 --decoder-layers 6 --encoder-layers 6 \
 --encoder-embed-dim 512 --decoder-embed-dim 512 \
@@ -36,5 +29,5 @@ CUDA_VISIBLE_DEVICES=1 nohup python train.py $DATA_BIN \
 --copy-attention --copy-attention-heads 1 \
 --no-ema \
 --use-encoder-classification \
---label-gen-loss-rate 1 \
-> log/ft_lang8_all_cged_cltc3_1.log &
+--seed 1 \
+> log/ft_lang8_all_cged_all_cltc7_7.log &
